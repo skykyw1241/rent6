96e0ba44ee9b32ed5a1ed1cb344a2bc053f26f0b
skykyw1241 <skykyw1241@naver.com>
2020-10-28 21:35:36 +0900

    configmap1

--- src/main/java/com/example/template/book/BookController.java
+++ src/main/java/com/example/template/book/BookController.java
@@ -32,24 +32,4 @@ public class BookController {
         return this.bookService.save(data);
     }
 
-    /*@Autowired
-    Environment env;
-    *//**
-     * env 혹은 설정 값을 가져오는 테스트 코드
-     *//*
-    @GetMapping("/env")
-    String getEnvSample() {
-
-        // 1. applicationContext 에서 가져오기
-        Environment envContext = Application.applicationContext.getEnvironment();
-        System.out.println(" applicationContext = " + envContext.getProperty("superuser.userId"));
-
-        // 2. Autowired Environment
-        System.out.println(" Autowired Environment = " + env.getProperty("superuser.userId"));
-
-        // 3. @Value
-        System.out.println(" @Value = " + superUserId);
-
-        return superUserId;
-    }*/
 }
--- src/main/java/com/example/template/delivery/DeliveryController.java
+++ src/main/java/com/example/template/delivery/DeliveryController.java
@@ -31,24 +31,4 @@ public class DeliveryController {
         return this.deliveryService.save(data);
     }
 
-    //@Value("${superuser.userId}")
-    //String superUserId;
-
-    @Autowired
-    Environment env;
-    @GetMapping("/env")
-    String getEnvSample() {
-
-        // 1. applicationContext 에서 가져오기
-        Environment envContext = Application.applicationContext.getEnvironment();
-        System.out.println(" applicationContext = " + envContext.getProperty("superuser.userId"));
-
-        // 2. Autowired Environment
-        System.out.println(" Autowired Environment = " + env.getProperty("superuser.userId"));
-
-        // 3. @Value
-        //System.out.println(" @Value = " + superUserId);
-
-        return env.getProperty("superuser.userId");
-    }
 }
